c1016b7391740c51bd27894c63e20cf9d69df6f7
johvik <johvik@users.noreply.github.com>
2017-06-24 20:38:40 +0200

    Update package.json

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('cave App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Sensors');
+    expect<any>(page.getParagraphText()).toEqual('Sensors');
   });
 });
